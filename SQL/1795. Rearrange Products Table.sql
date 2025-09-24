c59d49aaf3ce823cc88cd7194a150c90f0c947af
OscarWei61 <wei573434@gmail.com>
2025-09-24 15:35:52 -0500

    Create 1795. Rearrange Products Table.sql

---
+++ SQL/1795. Rearrange Products Table.sql
@@ -0,0 +1,24 @@
+# Write your MySQL query statement below
+with store1 as (
+    select s1.product_id, "store1" as store, store1 as price
+    from Products s1
+    where store1 is not null
+),
+
+ store2 as (
+    select product_id, "store2" as store, store2 as price
+    from Products s2
+    where store2 is not null
+),
+
+ store3 as (
+    select product_id, "store3" as store, store3 as price
+    from Products s3
+    where store3 is not null
+)
+
+select * from store1
+union all
+select * from store2
+union all
+select * from store3;
