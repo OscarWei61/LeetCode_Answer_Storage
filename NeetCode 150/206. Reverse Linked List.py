745cd9510fcf69d4381ad319008bcb8eb540f0d5
OscarWei61 <87572431+OscarWei61@users.noreply.github.com>
2026-04-02 16:33:11 -0500

    Create 206. Reverse Linked List.py

---
+++ NeetCode 150/206. Reverse Linked List.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def reverseList(self, head):
+        prev = None
+        curr = head
+        
+        while curr:
+            next_temp = curr.next
+            
+            curr.next = prev
+            
+            prev = curr
+            curr = next_temp
+            
+        return prev
